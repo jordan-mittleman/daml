baea1c2136e603bbb5df8ad7d92f9761a490da74
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2019-07-11 09:03:32 +0100

    enable old extension test

--- compiler/daml-extension/ci-tests.sh
+++ compiler/daml-extension/ci-tests.sh
@@ -2,13 +2,11 @@
 # Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set -eux
-DAML_EXTENSION_DIR=$1
-CHECK_JSON=$2
+set -euo pipefail
 
-cd $DAML_EXTENSION_DIR
+cd compiler/daml-extension
 
 : Checking JSON files for proper syntax
 for sf in *.json syntaxes/*.json; do
-  $CHECK_JSON $sf
+  cat $sf | jq '.'
 done
